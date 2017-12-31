589f5227c67c6ace38a381a1efcf86e1d7fd83f0
TheMartes <matt.buncek@gmail.com>
2017-12-31 23:21:41 +0100

    Added application design

--- Git Ignore Generator/Git Ignore Generator/Form1.Designer.cs
+++ Git Ignore Generator/Git Ignore Generator/Form1.Designer.cs
@@ -28,12 +28,363 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "actionscript",
+            "ada",
+            "agda",
+            "android",
+            "anjuta",
+            "appceleratortitanium",
+            "archives",
+            "archlinuxpackages",
+            "autotools",
+            "bricxcc",
+            "c",
+            "c++",
+            "cakephp",
+            "cfwheels",
+            "chefcookbook",
+            "clojure",
+            "cloud9",
+            "cmake",
+            "codeigniter",
+            "codekit",
+            "commonlisp",
+            "composer",
+            "concrete5 ",
+            "coq ",
+            "craftcms",
+            "cvs ",
+            "dart ",
+            "darteditor ",
+            "delphi",
+            "dm",
+            "dreamweaver",
+            "drupal",
+            "eagle",
+            "eclipse",
+            "eiffelstudio",
+            "elisp",
+            "elixir",
+            "emacs",
+            "ensime",
+            "episerver",
+            "erlang",
+            "espresso",
+            "expressionengine",
+            "extjs",
+            "fancy",
+            "finale",
+            "flexbuilder",
+            "forcedotcom",
+            "fortran",
+            "fuelphp",
+            "gcov",
+            "gitbook",
+            "go",
+            "gradle",
+            "grails",
+            "gwt",
+            "haskell",
+            "idris",
+            "igorpro",
+            "ipythonnotebook",
+            "java",
+            "jboss",
+            "jdeveloper",
+            "jekyll",
+            "jetbrains",
+            "joomla",
+            "jython",
+            "kate",
+            "kdevelop4",
+            "kohana",
+            "labview",
+            "laravel",
+            "lazarus",
+            "leiningen",
+            "lemonstand",
+            "libreoffice",
+            "lilypond",
+            "linux",
+            "lithium",
+            "lua",
+            "lyx",
+            "magento",
+            "matlab",
+            "maven",
+            "mercurial",
+            "mercury",
+            "metaprogrammingsystem",
+            "meteor ",
+            "microsoftoffice",
+            "modelsim",
+            "momentics",
+            "monodevelop",
+            "nanoc",
+            "netbeans",
+            "nim",
+            "ninja",
+            "node",
+            "notepadpp",
+            "objective-c",
+            "ocaml",
+            "opa",
+            "opencart",
+            "oracleforms",
+            "osx",
+            "packer",
+            "perl",
+            "phalcon",
+            "playframework",
+            "plone",
+            "prestashop",
+            "processing",
+            "python",
+            "qooxdoo",
+            "qt",
+            "r",
+            "rails",
+            "redcar",
+            "redis",
+            "rhodesrhomobile",
+            "ros",
+            "ruby",
+            "rust",
+            "sass",
+            "sbt",
+            "scala",
+            "scons",
+            "scrivener",
+            "sdcc",
+            "seamgen",
+            "sketchup",
+            "slickedit",
+            "stella",
+            "sublimetext",
+            "sugarcrm",
+            "svn",
+            "swift",
+            "symfony",
+            "symphonycms",
+            "tags",
+            "tex",
+            "textmate",
+            "textpattern",
+            "tortoisegit",
+            "turbogears2",
+            "typo3",
+            "umbraco",
+            "unity",
+            "vagrant",
+            "vim",
+            "virtualenv",
+            "visualstudio",
+            "vvvv",
+            "waf",
+            "webmethods",
+            "windows",
+            "wordpress",
+            "xcode",
+            "xilinxise",
+            "xojo",
+            "yeoman",
+            "yii",
+            "zendframework",
+            "zephir"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 44);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 214);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 263);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(384, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(138, 44);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(97, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Add Comments";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(138, 67);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(82, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Save as .txt";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(408, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newWindowToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newWindowToolStripMenuItem
+            // 
+            this.newWindowToolStripMenuItem.Name = "newWindowToolStripMenuItem";
+            this.newWindowToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.newWindowToolStripMenuItem.Text = "New Window";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(145, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select Languages";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Optional presets";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select output path (your project root)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Open folder";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Please select you path";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(408, 298);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(424, 337);
+            this.Name = "Form1";
+            this.Text = "Git Ignore Generator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newWindowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
